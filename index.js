044d279e10a8f3d6f1884eaea5640337a1886370
seniordev12345 <hhu5731@outlook.com>
2022-06-24 07:02:28 -0700

    feat: set port

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT || 80;
+const port = 3000;
 
 // server listening
-server.listen(80, () => {
-  console.log(`Server running on port 80`);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
