2dc9389e186923a1dc0e1985de04a73a8e4d8243
seniordev12345 <hhu5731@outlook.com>
2022-06-24 12:18:59 -0700

    feat: removed issues

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
-// require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const QR = require("qrcode");
-const User = require("./model/user");
-const ConnectedDevice = require("./model/connectedDevice");
-const QRCode = require("./model/qrCode");
 const cors = require("cors");
 
 const app = express();
@@ -27,88 +23,6 @@ const corsOpts = {
 };
 
 app.use(cors(corsOpts));
-
-app.post("/register", async (req, res) => {
-  // Our register logic starts here
-
-  try {
-    // Get user input
-    const { first_name, last_name, email, password } = req.body;
-
-    // Validate user input
-    if (!(email && password && first_name && last_name)) {
-      res.status(400).send("All input is required");
-    }
-
-    // check if user already exist
-    // Validate if user exist in our database
-    const oldUser = await User.findOne({ email });
-
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
-
-    // Encrypt user password
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    // Create user in our database
-    const user = await User.create({
-      first_name,
-      last_name,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
-      password: encryptedPassword,
-    });
-
-    // Create token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
-
-    // return new user
-    res.status(201).json({ token });
-  } catch (err) {
-    console.log(err);
-  }
-  // Our register logic ends here
-});
-
-app.post("/login", async (req, res) => {
-  // Our login logic starts here
-  try {
-    // Get user input
-    const { email, password } = req.body;
-
-    // Validate user input
-    if (!(email && password)) {
-      res.status(400).send("All input is required");
-    }
-
-    // Validate if user exist in our database
-    const user = await User.findOne({ email });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-
-      // user
-      return res.status(200).json({ token });
-    }
-    return res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
-  // Our login logic ends here
-});
 
 app.post("/qr/generate", async (req, res) => {
   try {
@@ -141,11 +55,10 @@ app.post("/qr/generate", async (req, res) => {
 
 app.post("/qr/scan", async (req, res) => {
   try {
-    // const { token, deviceInformation } = req.body;
     const { token } = req.body;
 
-    if (!token && !deviceInformation) {
-      res.status(400).send("Token and deviceInformation is required");
+    if (!token) {
+      res.status(400).send("Token is required");
     }
 
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
