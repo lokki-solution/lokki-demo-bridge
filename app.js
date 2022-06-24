be919531d68b336d4910e962201589c3df02d742
seniordev12345 <hhu5731@outlook.com>
2022-06-23 19:02:59 -0700

    feat: implemented qr genenrator and qr scan

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-require("./config/database").connect();
+// require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -7,10 +7,26 @@ const QR = require("qrcode");
 const User = require("./model/user");
 const ConnectedDevice = require("./model/connectedDevice");
 const QRCode = require("./model/qrCode");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 
 app.post("/register", async (req, res) => {
   // Our register logic starts here
@@ -96,6 +112,7 @@ app.post("/login", async (req, res) => {
 
 app.post("/qr/generate", async (req, res) => {
   try {
+    console.log(req.body);
     const { userId } = req.body;
 
     // Validate user input
@@ -103,26 +120,9 @@ app.post("/qr/generate", async (req, res) => {
       res.status(400).send("User Id is required");
     }
 
-    const user = await User.findById(userId);
-
-    // Validate is user exist
-    if (!user) {
-      res.status(400).send("User not found");
-    }
-
-    const qrExist = await QRCode.findOne({ userId });
-
-    // If qr exist, update disable to true and then create a new qr record
-    if (!qrExist) {
-      await QRCode.create({ userId });
-    } else {
-      await QRCode.findOneAndUpdate({ userId }, { $set: { disabled: true } });
-      await QRCode.create({ userId });
-    }
-
     // Generate encrypted data
     const encryptedData = jwt.sign(
-      { userId: user._id },
+      { userId: userId },
       process.env.TOKEN_KEY,
       {
         expiresIn: "1d",
@@ -133,7 +133,7 @@ app.post("/qr/generate", async (req, res) => {
     const dataImage = await QR.toDataURL(encryptedData);
 
     // Return qr code
-    return res.status(200).json({ dataImage });
+    return res.status(200).json({ dataImage, encryptedData });
   } catch (err) {
     console.log(err);
   }
@@ -141,7 +141,8 @@ app.post("/qr/generate", async (req, res) => {
 
 app.post("/qr/scan", async (req, res) => {
   try {
-    const { token, deviceInformation } = req.body;
+    // const { token, deviceInformation } = req.body;
+    const { token } = req.body;
 
     if (!token && !deviceInformation) {
       res.status(400).send("Token and deviceInformation is required");
@@ -149,46 +150,12 @@ app.post("/qr/scan", async (req, res) => {
 
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
 
-    const qrCode = await QRCode.findOne({
-      userId: decoded.userId,
-      disabled: false,
-    });
-
-    if (!qrCode) {
-      res.status(400).send("QR Code not found");
-    }
-
-    const connectedDeviceData = {
-      userId: decoded.userId,
-      qrCodeId: qrCode._id,
-      deviceName: deviceInformation.deviceName,
-      deviceModel: deviceInformation.deviceModel,
-      deviceOS: deviceInformation.deviceOS,
-      deviceVersion: deviceInformation.deviceVersion,
-    };
-
-    const connectedDevice = await ConnectedDevice.create(connectedDeviceData);
-
-    // Update qr code
-    await QRCode.findOneAndUpdate(
-      { _id: qrCode._id },
-      {
-        isActive: true,
-        connectedDeviceId: connectedDevice._id,
-        lastUsedDate: new Date(),
-      }
-    );
-
-    // Find user
-    const user = await User.findById(decoded.userId);
-
-    // Create token
-    const authToken = jwt.sign({ user_id: user._id }, process.env.TOKEN_KEY, {
+    const authToken = jwt.sign({ user_id: decoded.userId }, process.env.TOKEN_KEY, {
       expiresIn: "2h",
     });
 
     // Return token
-    return res.status(200).json({ token: authToken });
+    return res.status(200).json({ token: authToken, userId: decoded.userId });
   } catch (err) {
     console.log(err);
   }
