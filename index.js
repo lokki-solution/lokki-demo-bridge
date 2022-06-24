493642548caceddeb93ed6e7b105c7733a86087e
seniordev12345 <hhu5731@outlook.com>
2022-06-24 06:55:21 -0700

    feat: set port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT || 80;
 
 // server listening
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.listen(80, () => {
+  console.log(`Server running on port 80`);
 });
