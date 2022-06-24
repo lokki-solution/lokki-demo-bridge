8d7aca30d31bb538cae70847e0d1f8764c6e69a5
seniordev12345 <hhu5731@outlook.com>
2022-06-24 07:29:09 -0700

    feat: set port

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const port = 3000;
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
 
 // server listening
 server.listen(port, () => {
